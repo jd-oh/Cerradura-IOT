1b5c589ac3960d7506b988acfcb763e57ba39ba6
Juan David Osorio <72204602+jd-oh@users.noreply.github.com>
2023-12-07 15:04:19 -0500

    Nueva comunicacion con topicos, comandos del bot y algunas verificaciones

--- buttonBot.py
+++ buttonBot.py
@@ -1,40 +1,98 @@
+# Importar las bibliotecas necesarias
 import telebot
 import requests
 import subprocess
+import paho.mqtt.client as mqtt
 
+# Token del bot de Telegram
 BOT_TOKEN = '6965114657:AAEg9K0K2xYrrz_M3y4P3TXktaPRcLyTGO0'  
+
+# Crear una instancia del bot de Telegram
 bot = telebot.TeleBot(BOT_TOKEN)
 
-@bot.message_handler(commands=['start'])
+# Crear un cliente MQTT
+cliente = mqtt.Client()
+
+# Conectar el cliente MQTT al broker
+cliente.connect('91.121.93.94', 1883, 60)
+
+# Iniciar el loop del cliente MQTT para procesar los mensajes y mantener la conexión
+cliente.loop_start()
+
+# Definir el topic MQTT para la configuración
+topic="CERRADURAIOT/CONFIG"
+
+# Manejador para el comando /iniciar
+@bot.message_handler(commands=['iniciar'])
 def send_welcome(message):
+    # Crear un teclado inline con botones para abrir y cerrar la cerradura, y tomar una foto
     keyboard = telebot.types.InlineKeyboardMarkup()
     keyboard.row(
-        
         telebot.types.InlineKeyboardButton('Abrir cerradura', callback_data='open_lock'),
         telebot.types.InlineKeyboardButton('Cerrar cerradura', callback_data='close_lock'),
         telebot.types.InlineKeyboardButton('Tomar foto', callback_data='photo')
-        
     )
+    # Enviar un mensaje con el teclado inline
     bot.send_message(message.chat.id, 'Por favor elige:', reply_markup=keyboard)
 
+# Manejador para el comando /ayuda
+@bot.message_handler(commands=['ayuda'])
+def send_help(message):
+    # Definir el texto de ayuda
+    help_text = """
+    Aquí están los comandos que puedes usar:
+    - /iniciar: Inicia la interacción con el bot. Te muestra un menú con las opciones disponibles.
+    - /tiempoParaAlertar <segundos>: Cambia el tiempo después del cual se envían las alertas cuando la cerradura está abierta.
+    - /tiempoEntreAlertas <segundos>: Cambia el intervalo de tiempo entre las alertas.
+    """
+    # Enviar el texto de ayuda
+    bot.reply_to(message, help_text)
+
+# Manejador para las acciones de los botones del teclado inline
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     if call.data == 'open_lock':
-        response = requests.get('http://localhost:5000/open_lock')
-        bot.answer_callback_query(call.id, 'La cerradura se ha abierto')
+        # Verificar si la cerradura ya está abierta
+        lock_status = requests.get('http://localhost:5000/lock_status').json()
+        if lock_status['is_open']:
+            bot.answer_callback_query(call.id, 'La cerradura ya está abierta')
+        else:
+            response = requests.get('http://localhost:5000/open_lock')
+            bot.answer_callback_query(call.id, 'La cerradura se ha abierto')
     elif call.data == 'close_lock':
-        response = requests.get('http://localhost:5000/close_lock')
-        bot.answer_callback_query(call.id, 'La cerradura se ha cerrado')
+        # Verificar si la cerradura ya está cerrada
+        lock_status = requests.get('http://localhost:5000/lock_status').json()
+        if not lock_status['is_open']:
+            bot.answer_callback_query(call.id, 'La cerradura ya está cerrada')
+        else:
+            response = requests.get('http://localhost:5000/close_lock')
+            bot.answer_callback_query(call.id, 'La cerradura se ha cerrado')
     elif call.data == 'photo':
-        #print(call.message.chat.id)
-        #return
+        # Tomar una foto y enviarla
         subprocess.run(["termux-camera-photo","-c", "1","foto.jpg"])
         subprocess.run(["mogrify" ,"-quality" ,"60","foto.jpg"])
         bot.send_photo(call.message.chat.id, photo=open('foto.jpg', 'rb'))
         bot.answer_callback_query(call.id, 'Se ha enviado la foto')
-        #return "enviado"
-        #response = requests.get('http://localhost:5000/photo')
-        #bot.answer_callback_query(call.id, 'Foto tomada')
 
+# Manejador para el comando /tiempoParaAlertar
+@bot.message_handler(commands=['tiempoParaAlertar'])
+def set_alert_delay(message):
+    # El primer argumento del mensaje es el comando, el segundo es el valor
+    _, value = message.text.split()
+    # Publicar el nuevo valor en el topic de configuración 
+    cliente.publish(topic, 'Tiempo de alerta de cerradura abierta ha cambiado a:{}'.format(value))
+    # Confirmar al usuario que el valor ha sido cambiado en el chat
+    bot.reply_to(message, 'Se ha configurado el retraso de la alerta a {} segundos'.format(value))
+
+# Manejador para el comando /tiempoEntreAlertas
+@bot.message_handler(commands=['tiempoEntreAlertas'])
+def set_alert_interval(message):
+    # El primer argumento del mensaje es el comando, el segundo es el valor
+    _, value = message.text.split()
+    # Publicar el nuevo valor en el topic de configuración
+    cliente.publish(topic, 'Tiempo entre alertas de cerradura abierta ha cambiado a:{}'.format(value))
+    # Confirmar al usuario que el valor ha sido cambiado en el chat
+    bot.reply_to(message, 'Se ha configurado el intervalo de la alerta a {} segundos'.format(value))
+
+# Iniciar el bot
 bot.polling()
-
--- server.py
+++ server.py
@@ -1,21 +1,96 @@
-from flask import Flask, jsonify
+from threading import Thread
+from time import sleep
+from flask import Flask, jsonify, request
+import paho.mqtt.client as mqtt
+import time
+import requests
 
 app = Flask(__name__)
 
+# Crear una lista para guardar los mensajes de alerta
+alertas_log = []
+
+
+lock_open = False
+lock_open_time = None
+last_alert_time = None
+alert_delay = 30
+alert_interval = 20
+
+def check_lock_status():
+    global lock_open
+    global lock_open_time
+    global last_alert_time
+    global alert_interval  # Asegúrate de tener esta variable global
+    while True:
+        if lock_open and time.time() - lock_open_time > alert_delay:
+            if last_alert_time is None or time.time() - last_alert_time > alert_interval:
+                tiempo_abierta = int(time.time() - lock_open_time)
+                alerta = "La cerradura ha estado abierta por más de {} segundos: ".format(tiempo_abierta) + time.strftime("%d/%m/%y") + " a las " + time.strftime("%H:%M:%S")
+                alertas_log.append(alerta)
+                cliente.publish(topic_alertas, alerta)
+                last_alert_time = time.time()
+        sleep(1)
+
+
+def on_publish(client,userdata,result):
+    print("conexión exitosa")
+    print("Client {}\n UserData {}\n Result".format(client,userdata,result))
+
+def on_message(client, userdata, message):
+    key, value = message.payload.decode().split(':')
+    if key == 'Tiempo de alerta de cerradura abierta ha cambiado a':
+        global alert_delay
+        alert_delay = int(value)
+    elif key == 'Tiempo entre alertas de cerradura abierta ha cambiado a':
+        global alert_interval
+        alert_interval = int(value)
+
+
+
 @app.route('/open_lock', methods=['GET'])
 def open_lock():
-    
+    global lock_open
+    global lock_open_time
+    lock_open = True
+    lock_open_time = time.time()
+    alerta = "La cerradura se ha abierto en la fecha: "+time.strftime("%d/%m/%y")+" a las "+time.strftime("%H:%M:%S")
+    alertas_log.append(alerta)
+    cliente.publish(topic, alerta)
     return jsonify(message="La cerradura se ha abierto")
 
 @app.route('/close_lock', methods=['GET'])
 def close_lock():
-    
+    global lock_open
+    lock_open = False
+    alerta = "La cerradura se ha cerrado en la fecha: "+time.strftime("%d/%m/%y")+" a las "+time.strftime("%H:%M:%S")
+    alertas_log.append(alerta)
+    cliente.publish(topic, alerta)
     return jsonify(message="La cerradura se ha cerrado")
 
-@app.route('/photo', methods=['GET'])
-def photo():
-    
-    return jsonify(message="Foto tomada")
+@app.route('/lock_status', methods=['GET'])
+def lock_status():
+    global lock_open
+    return jsonify(is_open=lock_open)
+
 
 if __name__ == '__main__':
+    # Iniciar el hilo que verifica el estado de la cerradura
+    Thread(target=check_lock_status).start()
+
+    broker = "91.121.93.94"
+    port = 1883
+    topic = "CERRADURAIOT"
+    topic_alertas = "CERRADURAIOT/ALERTAS"
+
+    cliente = mqtt.Client()
+    cliente.onpublish = on_publish
+    cliente.connect(broker,port,keepalive=60)
+
+    cliente.on_message = on_message
+    cliente.subscribe('CERRADURAIOT/CONFIG')
+
+
+    cliente.loop_start()
+
     app.run(debug=True)
